61c3401681c1cef9b49f9d19c39d688b1d1266c4
Mugo-Ndungu <twinnymugo@gmail.com>
2019-02-19 00:17:59 +0300

    Add changes to sqlalchemy_database_uri

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:aaronmichelle@localhost/pichsss'
-    SQLALCHEMY_TRACK_NOTIFICATIONS = False
-    SECRET_KEY = 'Paula1234!'
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://alex:aaronmichelle@localhost/pichsss'
+    SQLALCHEMY_TRACK_NOTIFICATIONS=False
+    SECRET_KEY='Paula1234!'
+    UPLOADED_PHOTOS_DEST='app/static/photos'
     # email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'twinnymugo@gmail.com'
-    MAIL_PASSWORD = 'Paula1234!'
-    SUBJECT_PREFIX = 'BLOG HUB'
-    SENDER_EMAIL  = 'twinnymugo@gmail.com'
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME='twinnymugo@gmail.com'
+    MAIL_PASSWORD='Paula1234!'
+    SUBJECT_PREFIX='BLOG HUB'
+    SENDER_EMAIL='twinnymugo@gmail.com'
 
 
 class ProdConfig(Config):
@@ -25,7 +25,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URI")
 
 class DevConfig(Config):
     '''
